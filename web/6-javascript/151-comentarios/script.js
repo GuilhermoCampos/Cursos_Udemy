ecc9002f2ed1a1d4e742b38c90db0833d8563b72
Guilhermo Campos <gsoldier95@yahoo.com>
2022-08-27 10:11:39 -0300

    web 151

---
+++ web/6-javascript/151-comentarios/script.js
@@ -0,0 +1,9 @@
+// Comentário em JavaScript
+
+// Alerta ao iniciar a pagina
+alert('Welcome')
+/*
+    Selecionar um Elemento no DOM
+    Atualizar o valor desse elemento com uma string
+*/
+document.getElementById('nome').value = 'Oi'
